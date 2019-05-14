50054d738049b2edb2d3b48277c8ca0903dcc519
Victor Gonzalez <gonzalez@crahi.upc.edu>
2019-05-14 18:46:10 +0200

    change settings-heroku

--- radio/settings_heroku.py
+++ radio/settings_heroku.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
